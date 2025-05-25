d692fbb3aaa389f11354bb3504a03516db0fb775
calixtonascimento <calixto.nascimento@chavesnamao.com.br>
2025-05-25 20:48:18 -0300

    add: instalação do codeigniter 3 e configurações iniciais

---
+++ application/controllers/Teste.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Teste extends CI_Controller {
+
+    public function index(){
+        $this->load->database();
+        var_dump($this->db);
+    }
+}

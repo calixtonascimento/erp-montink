651186436c0bca9f2aea3d760fe0b221d9a2a8a6
calixtonascimento <calixto.nascimento@chavesnamao.com.br>
2025-05-25 20:51:31 -0300

    improve: controller de teste e comunicação com banco de dados

--- application/controllers/Teste.php
+++ application/controllers/Teste.php
@@ -5,6 +5,10 @@ class Teste extends CI_Controller {
 
     public function index(){
         $this->load->database();
-        var_dump($this->db);
+        if($this->db->conn_id){
+            echo "Conexão com o banco de dados bem-sucedida!";
+        } else {
+            echo "Falha na conexão com o banco de dados!";
+        }
     }
 }
